25543d6e87dec60de6bb3df4bf912984d7ab2244
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2018-10-31 15:55:15 +0300

    zsh: add function to define lazy completions for Go programs
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ zsh/00-functions.zsh
@@ -0,0 +1,18 @@
+add_lazy_completion() {
+	local cmd="$1"
+
+	compdef _${cmd}_load_completion ${cmd}
+
+	${cmd}() {
+		_${cmd}_load_completion
+		$0 "$@"
+	}
+
+	_${cmd}_load_completion() {
+		unfunction ${cmd}
+		unfunction _${cmd}_load_completion
+		compdef -d ${cmd}
+
+		source <(${cmd} completion zsh)
+	}
+}
--- zsh/12-commands.zsh
+++ zsh/12-commands.zsh
@@ -11,56 +11,6 @@ fi
 if (( $+commands[jump] )); then eval "$(jump shell)"; fi
 if (( $+commands[direnv] )); then eval "$(direnv hook zsh)"; fi
 
-# Lazy load kubectl completions
-if (( $+commands[kubectl] )); then
-	compdef _kubectl_load_completion kubectl
-
-	kubectl() {
-		_kubectl_load_completion
-		$0 "$@"
-	}
-
-	_kubectl_load_completion() {
-		unfunction kubectl
-		unfunction _kubectl_load_completion
-		compdef -d kubectl
-
-		source <(kubectl completion zsh)
-	}
-fi
-
-# Lazy load doctl completions
-if (( $+commands[doctl] )); then
-	compdef _doctl_load_completion doctl
-
-	doctl() {
-		_doctl_load_completion
-		$0 "$@"
-	}
-
-	_doctl_load_completion() {
-		unfunction doctl
-		unfunction _doctl_load_completion
-		compdef -d doctl
-
-		source <(doctl completion zsh)
-	}
-fi
-
-# Lazy load helm completions
-if (( $+commands[helm] )); then
-	compdef _helm_load_completion helm
-
-	helm() {
-		_helm_load_completion
-		$0 "$@"
-	}
-
-	_helm_load_completion() {
-		unfunction helm
-		unfunction _helm_load_completion
-		compdef -d helm
-
-		source <(helm completion zsh)
-	}
-fi
+for cmd in kubectl doctl helm; do
+	if (( $+commands[$cmd] )); then add_lazy_completion $cmd; fi
+done
