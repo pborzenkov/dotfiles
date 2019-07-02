4d3f479328960ca10b08805b85bedfc53b2b8de1
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2019-07-02 11:27:58 +0300

    zsh: add 'noglob' prefix for git
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- zsh/10-aliases.zsh
+++ zsh/10-aliases.zsh
@@ -4,6 +4,6 @@ alias la="ls -G -a"
 alias sshfa='ssh -o ForwardAgent=yes'
 alias sshvp='ssh -o ProxyCommand="nc -X connect -x acronis.sw.ru:80 %h %p"'
 
-for c in find fd rg curl http open youtube-dl;
+for c in find fd rg curl http open youtube-dl git;
 	alias $c="noglob $c"
 
