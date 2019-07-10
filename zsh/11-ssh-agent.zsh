3027aab86a26f4fa3cc9470d1b7b293c73e816be
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2019-07-10 11:35:27 +0300

    zsh: remove ssh-agent config
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- zsh/11-ssh-agent.zsh
+++
@@ -1,6 +0,0 @@
-# On local host use gpg-agent, on remote host use forwarded agent socket
-if [ -z "${SSH_CONNECTION}" ]; then
-	export GPG_TTY=$(tty)
-	export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-	gpgconf --launch gpg-agent
-fi
