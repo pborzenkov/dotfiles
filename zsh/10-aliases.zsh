dcb301f6adbdc2c831208c9e86e22b92c467f109
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2019-08-22 11:34:55 +0300

    zsh: remove SSH VZ alias
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- zsh/10-aliases.zsh
+++ zsh/10-aliases.zsh
@@ -2,7 +2,6 @@ alias l="ls -G"
 alias ll="ls -G -l"
 alias la="ls -G -a"
 alias sshfa='ssh -o ForwardAgent=yes'
-alias sshvp='ssh -o ProxyCommand="nc -X connect -x acronis.sw.ru:80 %h %p"'
 
 for c in find fd rg curl http open youtube-dl git;
 	alias $c="noglob $c"
